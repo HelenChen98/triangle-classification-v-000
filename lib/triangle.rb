9c3b10348fadcd68168222540b528c56ee4b829c
Karen C <chenyanhuan66@163.com>
2018-08-20 08:24:03 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,26 @@
 class Triangle
-  attr_accessor :a, :b, :c
+  attr_accessor :sum1, :sum2, :sum3, :a, :b, :c, :valid
 
   def initialize(a,b,c)
+    @sum1 = a+b
+    @sum2 = a+c
+    @sum3 = b+c
     @a = a
-    @b = b
-    @c = c
+    @b = b 
+    @c = c 
+
+    if a <= 0 || b <= 0 || c <= 0
+      @valid = false
+    elsif @sum1 < c || @sum2 < b || @sum3 < a
+      @valid = false
+    else
+      @valid = true
+    end
   end
+  
 
   def kind
-    array = [@a, @b , @c]
-    if @a + @b <= @c || @a + @c <= @b || @b + @c <= @a || @a <= 0 || @b <= 0 || @c <= 0
+    if @valid = false
       raise TriangleError
     else
       if @a = @b = @c
